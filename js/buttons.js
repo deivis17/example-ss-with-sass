ce0ad4c66c3edf59aa675db06f05faa39ce6139b
deivis17 <marchandeivis@gmail.com>
2023-01-20 17:06:42 -0400

    Nav Error Solved and Sticky position added

--- js/buttons.js
+++ js/buttons.js
@@ -1,5 +1,6 @@
 const showBtn = document.querySelector('.show-btn');
-const minMenu = document.querySelector('.minMenu');
+const minMenu = document.querySelector('.min-menu');
+console.log(showBtn, minMenu);
 
 showBtn.addEventListener('click',()=>{
     minMenu.classList.toggle('show')
